9f74d0b135bca61c7906e66a4e791e8b941fca1c
Andrew Kubis <akubis18@gmail.com>
2019-10-08 00:18:46 -0400

    Adding initial transform method to convert raw sheets object to a dynamicall ytranslated objec

--- services/gapi.js
+++ services/gapi.js
@@ -83,6 +83,7 @@ var GAPI = (function() {
     
     async function getSheetNames(spreadsheetId) {
         const sheetsPropertyArray = (await getSheetsProperties(spreadsheetId)).data.sheets;
+        // return sheetsPropertyArray;
         const names = [];
         sheetsPropertyArray.forEach((dict) => {
             names.push(dict.properties.title);
@@ -91,7 +92,6 @@ var GAPI = (function() {
     }
 
     async function getSheetsProperties(spreadsheetId) {
-        console.log("getSheets.");
         const gsapi = google.sheets({version: 'v4', auth: client});
         const query = {
             spreadsheetId: spreadsheetId,
@@ -106,13 +106,113 @@ var GAPI = (function() {
         const gsapi = google.sheets({version: 'v4', auth: client})    
         const fitnessSheet = {
             spreadsheetId: '1-8Pn3RysJRxDPzqMSiHd6aRQXJyPrkjbBzBOD-29vyY',
-            range: 'BB Decline Bench!A1:B5'
+            range: 'BB Decline Bench!A1:B2'
         };    
         return await gsapi.spreadsheets.get(fitnessSheet);
     };
 
+    async function test() {
+        console.log("TEST START.");
+        const gsapi = google.sheets({version: 'v4', auth: client})    
+        const fitnessSheet = {
+            spreadsheetId: '1-8Pn3RysJRxDPzqMSiHd6aRQXJyPrkjbBzBOD-29vyY',
+            range: 'BB Decline Bench!A1:B'
+        };    
+        const res = await gsapi.spreadsheets.values.get(fitnessSheet);
+        console.log("TEST COMPLETED.");
+        return res;
+    }
+
+    async function getSheetData(id, sheetName) {
+        const gsapi = google.sheets({version: 'v4', auth: client})   
+        const fitnessSheet = {
+            spreadsheetId: id,
+            range: sheetName + '!A:E'
+        };
+        return (await gsapi.spreadsheets.values.get(fitnessSheet)).data.values;
+    }
+
+    function shouldSplit(string) {
+        let splt = string.split('!');
+        if (splt.length > 1 && splt[splt.length - 1] === "split") {
+            return true;
+        } return false;
+    } 
+
+   function transformData(metaData, columns, rowsOfRows) {
+        /* Get the columns that need to be split */
+        const splittableColumns = [];
+        let final = [];
+        for (let i = 0; i < columns.length; i++) {
+            if (shouldSplit(columns[i])) {
+                splittableColumns.push(i);
+                columns[i] = columns[i].slice(0, -6);
+            }
+        }
+
+        rowsOfRows.forEach(row => {
+            let rowResult = {};
+            /*Begin adding splitted items*/
+            let maxSplit = 0;
+            splittableColumns.forEach(index => 
+                maxSplit = Math.max(row[index].split(',').length, maxSplit)
+            );
+
+            let splittedAttributes = [];
+            for (let i = 0; i < maxSplit; i++) {
+                let item = {};                
+                splittableColumns.forEach(index => {
+                    item[columns[index]] = get(row[index].split(','), i);
+                });
+                splittedAttributes.push(item);
+            }
+            rowResult["sets"] = splittedAttributes;
+
+             /*Begin adding rest of the non-split items*/
+            for (let i = 0; i < columns.length; i++) {
+                if (!(splittableColumns.includes(i))) {
+                    rowResult[columns[i]] = row[i];
+                }
+            }
+            final.push(rowResult);
+        });
+        return final;
+    }
+
+    // Return null rather than out of bounds exception
+    function get(array, index) {
+        if (index > array.length - 1) {
+            return null;
+        } return array[index];
+    }
+
+    /*
+       {sheetName1: 
+            [[metadata]]
+            [[col1], [col2!data], [col3]]
+            [[a1], [a2a,a2b,a2c], [a3]]
+            [[b1], [b2a,b2b,b2c], [b3]]
+            ....
+       , sheetName2:
+            ...
+       }
+    */
+    async function getAllData(id) {
+        var d = Date.now();
+        const sheetNames = await getSheetNames(id);
+        let result = {};
+        /* Make all requests and wait to resolve */
+        await Promise.all(
+            sheetNames.map(async (name) => {
+                result[name] = await getSheetData(id, name);
+            })
+        );
+        return result;
+    }
+
     return {
-        authorize, gsRun, getSheetNames
+        authorize, gsRun, getSheetNames, test, getSheetData, transformData
     }
 }())
+
 module.exports = { GAPI }
